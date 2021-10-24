4e06a3779dad7a8f4778df0d5ae81fb86303ad55
CryptoArdy <cryptoardy@gmail.com>
2021-10-24 14:52:41 -0400

    adding the correct path to Header

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
         <div class="Header-main">
             <div class="Header-logo">
                 <h1>
-                    <a href="/spa/">
+                    <a href="https://cryptoardy.github.io/spa/">
                         100tifi.co
                     </a>
                 </h1>
